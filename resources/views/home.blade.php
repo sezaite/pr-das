cb3b0a55f4e71bf309686b3600ad3942b66dc41d
sezaite <martyna.sezaite@gmail.com>
2021-04-14 12:52:23 +0100

    just a little margin added

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">{{ __('Dashboard') }}</div>
 
-                <div class="card-body">
+                <div class="card-body" style="margin-top: 10px">
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
                             {{ session('status') }}
